f821990549e6e482aa865960d9396da2322af184
bethwel123 <bethwellangat654@gmail.com>
2024-09-12 02:18:36 +0300

    Migrations and alembic

---
+++ lib/migrations/versions/2f9bb0ab920a_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 2f9bb0ab920a
+Revises: 
+Create Date: 2024-09-12 01:58:28.546981
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2f9bb0ab920a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
